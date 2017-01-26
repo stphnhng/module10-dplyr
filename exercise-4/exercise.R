50a6c81a08b1d4edc9ae8656ff4cedeb2c7664e9
Stephen Hung <hung.stephen.yh@gmail.com>
2017-01-26 14:07:15 -0800

    Finished exercise 4

--- exercise-4/exercise.R
+++ exercise-4/exercise.R
@@ -1,36 +1,57 @@
 # Exercise 4: DPLYR and flights data
 
-# Install the nycflights13 package and read it in.  Require the dplyr package
+# Install the `nycflights13` package. Load (`library()`) the package.
+# You'll also need to load `dplyr`
 # install.packages("nycflights13")
+library(nycflights13)
+library(dplyr)
+
+# The data.frame `flights` should now be accessible to you.
+# Use functions to inspect it: how many rows and columns does it have?
+# What are the names of the columns?
+# use `??flights` to search for documentation on the data set (for what cols are)
+
+ncol(flights)
+nrow(flights)
+colnames(flights)
 
 
-# The data.frame flights should now be accessible to you.  View it,
-# and get some basic information about the number of rows/columns
+# Use `dplyr` to give the data frame a new column that is the amount of time gained in the air
 
+flights <- mutate(flights, time.gain = arr_delay - dep_delay)
 
-# Add a column that is the amount of time gained in the air
+# Use `dplyr` to sort your data.frame descending by the column you just created.
+# Remember to save this as a variable (or in the same one!)
 
+flights <- arrange(flights, -time.gain)
 
-# Sort your data.frame desceding by the column you just created
+# For practice, repeat the last 2 steps in a single statement using the pipe operator
 
+flights <- mutate(flights, time.gain = arr_delay - dep_delay) %>% arrange(-time.gain)
 
-# Try doing the last 2 steps in a single operation using the pipe operator
+# Make a histogram of the amount of gain using the `hist()` function
 
-
-# Make a histogram of the amount of gain using the `hist` command
-
+hist(flights$time.gain)
 
 # On average, did flights gain or lose time?
+# Note: use the `na.rm = TRUE` argument to remove NA values from your aggregation
 
+mean(flights$time.gain, na.rm = TRUE)
 
-# Create a data.frame of flights headed to seatac ('SEA'),
-# and only include the column you just created
+# Create a data.frame of flights headed to seatac ('SEA'), only including the
+# origin, destination, and the "gain" column you just created
 
+seatac.frame <- filter(flights, dest == "SEA") %>% select(origin,dest,time.gain)
 
 # On average, did flights to seatac gain or loose time?
 
+mean(seatac.frame$time.gain, na.rm = TRUE)
 
-# Retireve the air_time column for flights from JFK to SEA
+# Consider flights from JFK to SEA. What was the average, min, and max air time
+# of those flights? Bonus: use pipes to answer this question in one statement!
+# (and don't show any other data)
 
-
-# What was the average air time of those flights (in hours)?  What was the min/max?
+jfk.to.sea.time <- filter(flights, origin == "JFK", dest == "SEA") %>% select(air_time)
+mean(jfk.to.sea.time$air_time, na.rm = TRUE)
+min(jfk.to.sea.time$air_time, na.rm = TRUE)
+max(jfk.to.sea.time$air_time, na.rm = TRUE)
