abdd8681ddeeeed0fff73f5517536ebdb9b9776d
Stephen Hung <hung.stephen.yh@gmail.com>
2017-01-26 15:16:44 -0800

    Finished exercise 5

--- exercise-5/exercise.R
+++ exercise-5/exercise.R
@@ -1,19 +1,34 @@
 # Exercise 5: DPLYR Grouped Operations
 
-# Install the nycflights13 package and read it in. Require the dplyr package.
-# install.packages("nycflights13")
+# Install the `nycflights13` package. Load (`library()`) the package.
+# You'll also need to load `dplyr`
+#install.packages("nycflights13")  # should be done already
+library(nycflights13)
+library(dplyr)
 
-
-# In which month was the average departure delay the greatest?
+# What was the average department delay in each month?
+# Save this as a data frame `dep.delay.by.month`
 # Hint: you'll have to perform a grouping operation before summarizing your data
 
+dep.delay.by.month <- group_by(flights, month) %>% summarize(mean = mean(dep_delay, na.rm = TRUE))
 
-# If you create a data.frame with the columns "month", and "delay" above, you should be able to create
-# a scatterplot by passing it to the 'plot' function
+# Which month had the greatest average departure delay?
 
+max(dep.delay.by.month$mean)
 
-# In which airport were the average arrival delays the highest?
+# If your above data frame contains the columns "month", and "delay", you can create
+# a scatterplot by passing that data frame to the 'plot()' function
+
+plot(dep.delay.by.month)
+
+# To which destinations were the average arrival delays the highest?
 # Hint: you'll have to perform a grouping operation before summarizing your data
+
+arr.delay.frame <- group_by(flights, dest) %>% summarize(mean.arrdelay = mean(arr_delay, na.rm = TRUE))
+max.delay <- max(arr.delay.frame$mean.arrdelay, na.rm = TRUE)
+arr.delay.frame$dest[arr.delay.frame$mean.arrdelay == max.delay]
+
+# You can look up these airports in the `airports` data frame!
 
 
 
--- exercise-6/exercise.R
+++ exercise-6/exercise.R
@@ -3,10 +3,15 @@
 # Install the nycflights13 package and read it in. Require the dplyr package.
 # install.packages("nycflights13")
 
-
+library('nycflights13')
+library('dplyr')
 
 # Create a dataframe of the average arrival delay for each destination, then use left_join
 # to join on the "airports" dataframe, which has the airport info
+
+dep.delay.by.month <- group_by(flights, dest) %>% summarize(mean = mean(dep_delay, na.rm = TRUE))
+
+left_join(airports, dep.delay.by.month)
 
 
 # Create a dataframe of the average arrival delay for each airline, then use left_join
